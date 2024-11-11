7ecc9349246e5116adf29a88fc7a10631c1a6433
NotIcko <icodenev1984@abv.bg>
2024-11-11 21:30:21 +0200

    Initial commit

---
+++ PresentAte/PresentAte.Data/PresentAteDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentAte.Data
+{
+    public class PresentAteDbContext : IdentityDbContext
+    {
+        public PresentAteDbContext()
+        {
+            
+        }
+
+        public PresentAteDbContext(DbContextOptions<PresentAteDbContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
